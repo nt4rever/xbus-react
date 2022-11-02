38f8d56852b29a8896dfa18115916516d9797e73
nt4rever <levantanald@gmail.com>
2022-11-02 18:01:12 +0700

    init app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import MainLayout from "./layouts/MainLayout";
+import Main from "./views/Main";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Main />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
